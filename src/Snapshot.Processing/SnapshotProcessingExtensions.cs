b8fb9ddf3a1ea6428ea2ff149b03c46baf993e8b
Magic <131926685+magiccodingman@users.noreply.github.com>
2026-07-26 15:37:51 -0400

    Add standard processing builder extension

---
+++ src/Snapshot.Processing/SnapshotProcessingExtensions.cs
@@ -0,0 +1,25 @@
+using Snapshot.Protocol.Build;
+
+namespace Snapshot.Processing;
+
+public static class SnapshotProcessingExtensions
+{
+    public static SnapshotEngineBuilder UseStandardProcessing(
+        this SnapshotEngineBuilder builder,
+        Action<SnapshotProcessingOptions>? configure = null)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        var options = new SnapshotProcessingOptions();
+        configure?.Invoke(options);
+        return builder.UseProcessor(new StandardSnapshotProcessor(options));
+    }
+
+    public static SnapshotEngineBuilder UseStandardProcessing(
+        this SnapshotEngineBuilder builder,
+        SnapshotProcessingOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(options);
+        return builder.UseProcessor(new StandardSnapshotProcessor(options));
+    }
+}
